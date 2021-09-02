60876405ec5477fb953d2e5bb1d230762b004963
sahil <mdsahil0170@gmail.com>
2021-09-02 15:50:39 +0600

    Version 2

--- first.py
+++ first.py
@@ -1,5 +1,8 @@
 # there is a function which sum two noumber
-def two_num(a,b):
-    return a+b
+class class_s:
+    def two_sum(a,b):
+        return a+b
+    def two_unsum(a,b):
+        return a-b
 
-print(two_num(5,5))
+print(class_s.two_sum(5,5),class_s.two_unsum(5,5))
